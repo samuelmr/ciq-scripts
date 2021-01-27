f122a7fcbeed56b7b0cd39274109f5e5061eca0d
Samuel Rinnetmäki <samuel@iki.fi>
2021-01-27 09:18:36 +0200

    HOME and heredoc fixes

--- create-connectiq-shortcuts.sh
+++ create-connectiq-shortcuts.sh
@@ -1,24 +1,24 @@
 
 # Personal preferences
-DEVELOPER_KEY="~/garmin/developer_key.der"  # your developer key file
-SHELL_RC_FILE="~/.zshrc-test"               # depends on your shell
-DEFAULT_DEVICE=fenix5plus                   # default simulator device
-BIN_PATH=~/ciq-scripts                      # location of this package
-CIQ_SHORTCUT=~/connectiq                    # convenience shortcut to SDK
+DEVELOPER_KEY="$HOME/garmin/developer_key.der"  # your developer key file
+SHELL_RC_FILE="$HOME/.zshrc"                    # depends on your shell
+DEFAULT_DEVICE="fenix5plus"                     # default simulator device
+BIN_PATH="$HOME/ciq-scripts"                    # location of this package
+CIQ_SHORTCUT="$HOME/connectiq"                  # convenience shortcut to SDK
 
 # ConnectIQ SDK locations
-CIQ_PATH=~/Library/Application\ Support/Garmin/ConnectIQ/Sdks/connectiq-sdk
-CIQ_CONFIG_PATH="~/Library/Application\ Support/Garmin/ConnectIQ/current-sdk.cfg"
+CIQ_PATH="$HOME/Library/Application Support/Garmin/ConnectIQ/Sdks/connectiq-sdk"
+CIQ_CONFIG_PATH="$HOME/Library/Application Support/Garmin/ConnectIQ/current-sdk.cfg"
 
 # You can also set this to point to another SDK version instead of setting
 # the default SDK version in the SdkManager
-ln -s $CIQ_PATH $CIQ_SHORTCUT
+ln -s "$CIQ_PATH" "$CIQ_SHORTCUT" && echo "Symlinked Connect IQ SDK to '$CIQ_SHORTCUT'"
 
 # Set $PATH and create aliases
-echo <<EORC >> $SHELL_RC_FILE
-export CIQ_TARGET=\$DEFAULT_DEVICE
-export CIQ_KEYFILE=\$DEVELOPER_KEY
-export CIQ_HOME=`cat $CIQ_CONFIG_PATH`
+cat << EORC >> "$SHELL_RC_FILE" && echo "Edited $SHELL_RC_FILE"
+export CIQ_TARGET="$DEFAULT_DEVICE"
+export CIQ_KEYFILE="$DEVELOPER_KEY"
+export CIQ_HOME=\`cat "$CIQ_CONFIG_PATH"\`
 export PATH=\$PATH:~/connectiq/bin:"\${CIQ_HOME}bin"
 alias mc="$BIN_PATH/monkeyc.sh"
 alias md="$BIN_PATH/monkeydo.sh"
@@ -29,5 +29,3 @@ alias iqtest="$BIN_PATH/iqtest.sh"
 alias iqdevices='ls -1 "\${CIQ_HOME}../../devices"'
 
 EORC
-
-echo "Edited $SHELL_RC_FILE"
